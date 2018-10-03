39b828ac67cb4bd4ec51dce2ab2a4e0e999a6ff3
Jacob Parry <griever815@gmail.com>
2018-10-03 14:29:13 -0600

    added worker

--- elixir_playground_umbrella/apps/elvenhearth/lib/elvenhearth/application.ex
+++ elixir_playground_umbrella/apps/elvenhearth/lib/elvenhearth/application.ex
@@ -6,10 +6,12 @@ defmodule Elvenhearth.Application do
   use Application
 
   def start(_type, _args) do
+    import Supervisor.Spec
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: Elvenhearth.Worker.start_link(arg)
       # {Elvenhearth.Worker, arg},
+      worker(Elvenhearth.Repo, [])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
